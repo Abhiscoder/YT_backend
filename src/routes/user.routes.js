6856e51257fcbe307eeb224274d7678e0855ee12
Abhiscoder <abhishekkumar200m1@gmail.com>
2025-12-23 15:44:19 +0530

    login and logout

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
+import { varifyJWT } from "../middlewares/auth.middleware.js"
 
 const router = Router();
 
@@ -15,4 +16,8 @@ router.route("/register").post(upload.fields([
   }
 ]), registerUser)
 
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(varifyJWT, logoutUser)
 export default router;
