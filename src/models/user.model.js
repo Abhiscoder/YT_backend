d03b7f7d8987aa34c60875731a357281ba38bd97
Abhiscoder <abhishekkumar200m1@gmail.com>
2025-12-23 14:56:43 +0530

    postman dibuing

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
@@ -32,6 +32,7 @@ const userSchema = new mongoose.Schema(
     },
     coverImage: {
       type: String, //cloudinary URL
+      required: true,
     },
     watchHistory: [
       {
@@ -49,11 +50,18 @@ const userSchema = new mongoose.Schema(
   }, { timestamps: true }
 )
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+// userSchema.pre("save", async function (next) {
+//   if (!this.isModified("password")) return next();
+
+//   this.password = await bcrypt.hash(this.password, 10);
+//   next();
+// })
+
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
   this.password = await bcrypt.hash(this.password, 10)
-  next()
+
 })
 
 userSchema.methods.isPasswordCorrect = async function (password) {
@@ -74,7 +82,7 @@ userSchema.methods.generateAccessToken = function () {
     }
   )
 }
-userSchema.methods.generateRefreshToken = function () { 
+userSchema.methods.generateRefreshToken = function () {
   return jwt.sign(
     {
       _id: this._id,
